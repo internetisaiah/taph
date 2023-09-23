aa5e15083a40992cc1470ae6f2d0784252dc17b2
Isaiah Robinson <95643215+internetisaiah@users.noreply.github.com>
2023-09-23 10:27:43 +0200

    Adding looping to parser for maintainability

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ I created these scripts to help manage the static site generator (Antora) and th
 | Name                 | Go to file                                 | Description                                                                                                                              |
 |----------------------|--------------------------------------------|------------------------------------------------------------------------------------------------------------------------------------------|
 | `beautify_logs.py`   | [File](scripts/beautify.py)           | Converts Antora's JSON logs into a "beautified," interactive table, so docs maintainers can open flagged files directly from their preview window.                |
-| `cleanup_content.py` | [File](scripts/general.py)         | Runs all scripts in the [modules directory](./modules) which: adds the module name to Antora attributes, fixes errors with spaces and periods, fixes newlines, and fixes URLs formatting.                   |
+| `cleanup_content.py` | [File](scripts/edit.py)         | Runs all scripts in the [modules directory](./modules) which: adds the module name to Antora attributes, fixes errors with spaces and periods, fixes newlines, and fixes URLs formatting.                   |
 | `create_csv.py`      | [File](scripts/csv.py)              | Generates a CSV file with the following information for each document: title, section, subsection, and site URL.                                                   |
 | `minify_css.py`      | [File](scripts/minify.py)              | Merges all human-readable CSS files from the [UI bundle directory](./ui-bundle/css) into a single, minifed file (`site.css`), which improves front-end development workflows while increasing site responsiveness. |
 
@@ -28,7 +28,7 @@ cd <path-to-script-directory>/docs-scripts/scripts
 Next, make each script executable in your shell environment.
 
 ```shell
-chmod +x beautify.py general.py csv.py minify.py
+chmod +x beautify.py edit.py csv.py minify.py
 ```
 
 Since there's a [shebang](https://en.wikipedia.org/wiki/Shebang_(Unix)) on the first line of every script (`#!/usr/bin/env python3`), you don't need to invoke Python manually. Simply enter the path to the script you'd like to use. For example:
--- aph.py
+++ aph.py
@@ -4,45 +4,68 @@
 
 import subprocess
 import argparse
-from scripts.general import main as general
+from scripts.edit import main as general
 from scripts.beautify import main as beautify
 from scripts.minify import main as minify
 from scripts.csv import main as csv
 
-PROJECT_ROOT = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).decode('utf-8').strip()
-parser = argparse.ArgumentParser(description="Manage your Antora project with APH.")
+# Constants:
+# Dictionary mapping optional arguments to corresponding functions in './scripts'.
+APH_SCRIPTS = {
+    'general': general,
+    'beautify': beautify,
+    'minify': minify,
+    'csv': csv
+}
 
-# Argument(s)
-parser.add_argument("directory",
-                    nargs='?',
-                    default=PROJECT_ROOT,
-                    help="the antora project's root directory (default: the value of '%(default)s')")
-parser.add_argument("-g", "--general",
-                    help="a general cleanup script to add the relative module to each asciidoc attribute,"
-                         "add missing spaces after periods, fix newlines, and fix url strings",
-                    action="store_true")
-parser.add_argument("-b", "--beautify",
-                    help="beautifies the logs in 'logs/' by creating an asciidoc table with cross-references "
-                         "to each file that can be opened directly from the preview window",
-                    action="store_true")
-parser.add_argument("-m", "--minify",
-                    help="copies all css content from the ui bundle directory into a single, minified file: 'site.css'",
-                    action="store_true")
-parser.add_argument("-c", "--csv",
-                    help="creates a '.csv' file with all site urls which can"
-                         " be imported to one or more spreadsheets for docs reviews",
-                    action="store_true")
-# TODO: make 'quiet' command
+# Antora project's root directory.
+PROJECT_ROOT = subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).decode('utf-8').strip()
+
+
+# APH argument parser using Python's 'argparse'.
+def main():
+    parser = argparse.ArgumentParser(description="Manage your Antora project with APH.")
+
+    # Argument(s)
+    parser.add_argument("directory",
+                        nargs='?',
+                        default=PROJECT_ROOT,
+                        help="your antora project's root directory "
+                             "(default: the value of 'git rev-parse --show-toplevel')")
+    parser.add_argument("-q", "--quiet",
+                        help="suppresses all aph notifications",
+                        action="store_true")
+    parser.add_argument("-b", "--beautify",
+                        help="beautify your logs by creating an asciidoc table containing cross-references "
+                             "to each file which can be opened from your ide's preview window",
+                        action="store_true")
+    parser.add_argument("-m", "--minify",
+                        help="copy all css content from your ui bundle directory into a single, minified file: 'site.css'",
+                        action="store_true")
+    parser.add_argument("-c", "--csv",
+                        help="generate a '.csv' file containing the urls for the documents in your project's "
+                             "output directory which can be imported later into a spreadsheet",
+                        action="store_true")
+    parser.add_argument("-e", "--edit",
+                        help="make the following edits to each '.adoc' file in './docs/modules/': "
+                             "fix newlines, fix urls, "
+                             "add modules to asciidoc attributes,"
+                             "add missing spaces after periods",
+                        action="store_true")
+
+    args = parser.parse_args()
+
+    # Check the given optional argument(s) and run the matching function(s) from the 'aph_scripts' dictionary.
+    for arg, script in APH_SCRIPTS.items():
+        if getattr(args, arg):
+            script()
+            if not args.quiet:
+                print(f"{arg} successfully executed.")
+
+    # If no optional argument(s) given, notify user.
+    if not any([getattr(args, key) for key in APH_SCRIPTS.keys()]):
+        print("At least one optional argument is required for 'aph'. For more information, see '--help'.")
 
 
 if __name__ == "__main__":
-    args = parser.parse_args()
-
-    if args.general:
-        general()
-    elif args.beautify:
-        beautify()
-    elif args.minify:
-        minify()
-    elif args.csv:
-        csv()
+    main()
